e91a2b866249a29f8fdd17585ab3bb12c5215e63
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-20 21:35:00 +0100

    Refactoring part 1

--- src/index.js
+++ src/index.js
@@ -2,6 +2,7 @@ console.log(data);
 
 // WRITE YOUR CODE BELOW!
 
+// ---- Part 1 ---- //
 const dogListUl = document.querySelector(".dogs-list");
 for (let i = 0; i < data.length; i++) {
   const dogs = data[i];
@@ -16,10 +17,8 @@ for (let i = 0; i < data.length; i++) {
     const removeNoDogForm = document.querySelector(".main__dog-section");
     removeNoDogForm.remove();
     console.log(removeNoDogForm);
-    //create the dog card
-    //get the main section and append the dog main section
 
-    //Main dog section
+    //Create Section Tag
     const mainDog = document.createElement("section");
     mainDog.setAttribute("class", "main__dog-section");
 
@@ -32,40 +31,37 @@ for (let i = 0; i < data.length; i++) {
     //create img
     const dogImg = document.createElement("img");
     dogImg.setAttribute("class", "dog-img");
-    dogImg.image = dogs.image;
+    dogImg.src = dogs.image;
     mainDog.append(dogImg);
 
-    //create main dog section desc
+    //create main dog section
     const dogDesc = document.createElement("div");
     mainDog.append(dogDesc);
 
     //Create h3
     const dogBioH3 = document.createElement("h3");
-    dogBioH3.setAttribute("class", "dog-bio");
-    dogBioH3.innerHTML = dogs.bio;
+    dogBioH3.innerText = "Bio";
     mainDog.append(dogBioH3);
 
     //create p
     const dogParaEl = document.createElement("p");
+    dogParaEl.innerHTML = dogs.bio;
     mainDog.append(dogParaEl);
-
-    //create div for button section
-    const dogDivButton = document.createElement("div");
-    mainDog.append(dogDivButton);
 
     //create p tag "is naughty? yes!"
     const isGoodBoy = document.createElement("p");
-    isGoodBoy.innerText = "Is naughty? yes!";
+    isGoodBoy.innerText = "Is naughty? " + dogs.isGoodDog;
     mainDog.append(isGoodBoy);
-    //Link the main section with mainDof section
-    const main = document.querySelector(".main");
-    main.append(mainDog);
 
     //create button
     const dogButton = document.createElement("button");
     dogButton.setAttribute("class", "main__dog-section__btn");
-    dogButton.innerHTML = dogs.isGoodDog;
+    dogButton.innerText = "Good dog!";
     mainDog.append(dogButton);
+
+    //Link the main section with mainDof section
+    const main = document.querySelector(".main");
+    main.append(mainDog);
 
     /*let changeDogForm = 7;
 
