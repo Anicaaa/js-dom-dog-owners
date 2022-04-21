27ca49dca9dff3ee4fb9edfab43c324f223514da
Anica Rahman <anicarahman2009@yahoo.it>
2022-04-21 14:32:32 +0100

    Part 2 completed

--- src/index.js
+++ src/index.js
@@ -25,7 +25,7 @@ for (let i = 0; i < data.length; i++) {
     //create h2
     const dogNameH2 = document.createElement("h2");
     dogNameH2.setAttribute("class", "name-dog");
-    dogNameH2.innerHTML = dogs.name;
+    dogNameH2.innerText = dogs.name;
     mainDog.append(dogNameH2);
 
     //create img
@@ -48,9 +48,14 @@ for (let i = 0; i < data.length; i++) {
     dogParaEl.innerHTML = dogs.bio;
     mainDog.append(dogParaEl);
 
-    //create p tag "is naughty? yes!"
+    //create em "is naughty?"
+    const em = document.createElement("em");
+    em.innerText = "is naughty?";
+    mainDog.append(em);
+
+    //create p tag
     const isGoodBoy = document.createElement("p");
-    isGoodBoy.innerText = "Is naughty? " + dogs.isGoodDog;
+    isGoodBoy.innerText = dogs.isGoodDog;
     mainDog.append(isGoodBoy);
 
     //create button
@@ -59,15 +64,51 @@ for (let i = 0; i < data.length; i++) {
     dogButton.innerText = "Good dog!";
     mainDog.append(dogButton);
 
-    //Link the main section with mainDof section
+    //Link the main section with mainDog section
     const main = document.querySelector(".main");
     main.append(mainDog);
 
-    /*let changeDogForm = 7;
+    // ---- Part 2 ---- //
+    const addButton = document.querySelector(".dogs-list__button--add");
+    addButton.addEventListener("click", function () {
+      mainDog.innerHTML = "";
 
-  const addButton = document.querySelector(".dogs-list__button--add");
-  addButton.addEventListener("click", function () {
-    changeDogForm++;
-  });*/
+      const h2 = document.createElement("h2");
+      h2.innerText = "Add a new Dog";
+      mainDog.append(h2);
+
+      const form = document.createElement("form");
+      form.setAttribute("class", "form");
+
+      const inputText = document.createElement("input");
+      inputText.setAttribute("type", "text");
+      inputText.setAttribute("id", "name");
+      inputText.setAttribute("id", "name");
+      inputText.setAttribute("name", "name");
+      form.append(inputText);
+
+      const inputUrl = document.createElement("input");
+      inputUrl.setAttribute("type", "url");
+      inputUrl.setAttribute("id", "image");
+      inputUrl.setAttribute("name", "image");
+      form.append(inputUrl);
+
+      const textArea = document.createElement("textarea");
+      textArea.setAttribute("rows", "5");
+      textArea.setAttribute("id", "bio");
+      textArea.setAttribute("name", "bio");
+      form.append(textArea);
+
+      const submitButton = document.createElement("input");
+      submitButton.setAttribute("type", "submit");
+      submitButton.setAttribute("id", "submit");
+      submitButton.setAttribute("name", "submit");
+      submitButton.setAttribute("value", "Let's add a dog!");
+      submitButton.setAttribute("class", "form__button");
+      form.append(submitButton);
+
+      //Link the mainDog section with the form
+      mainDog.append(form);
+    });
   });
 }
